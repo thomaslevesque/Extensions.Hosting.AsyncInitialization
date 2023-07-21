c0800d60a07869c754d8b708e2aa7ffd272ad707
Alexander Röthinger <alexander1967muc@hotmail.com>
2023-07-21 20:10:33 +0200

    Added static method RunInitAsync() to wrap Host.RunAsync() with initialization and teardown

--- src/Extensions.Hosting.AsyncInitialization/Hosting/AsyncInitializationHostExtensions.cs
+++ src/Extensions.Hosting.AsyncInitialization/Hosting/AsyncInitializationHostExtensions.cs
@@ -32,5 +32,35 @@ namespace Microsoft.Extensions.Hosting
 
             await rootInitializer.InitializeAsync(cancellationToken);
         }
+
+
+        /// <summary>
+        /// Initializes and runs the application, by first calling all registered async initializers.
+        /// Upon completion, any registered initializers implementing <see cref="IAsyncTeardown"/> are called.
+        /// </summary>
+        /// <param name="host">The host.</param>
+        /// <param name="cancellationToken">Optionally propagates notifications that the operation should be cancelled</param>
+        /// <returns>A task that represents the initialization completion.</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="InvalidOperationException"></exception>
+        /// <exception cref="OperationCanceledException"></exception>
+        public static async Task RunInitAsync(this IHost host, CancellationToken cancellationToken = default)
+        {
+            if (host == null) throw new ArgumentNullException(nameof(host));
+
+            using var scope = host.Services.CreateScope();
+            var rootInitializer = scope.ServiceProvider.GetService<RootInitializer>()
+                ?? throw new InvalidOperationException("The async initialization service isn't registered, register it by calling AddAsyncInitialization() on the service collection or by adding an async initializer.");
+
+            try
+            {
+                await rootInitializer.InitializeAsync(cancellationToken).ConfigureAwait(false);
+                await host.RunAsync(cancellationToken).ConfigureAwait(false);
+            }
+            finally
+            {
+                await rootInitializer.TeardownAsync().ConfigureAwait(false);
+            }
+        }
     }
 }
