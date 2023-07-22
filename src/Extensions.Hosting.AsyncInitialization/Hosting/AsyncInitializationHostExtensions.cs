0c99c57d3f3e4c390579bcfdd6321bfff6647885
Alexander Röthinger <alexander1967muc@hotmail.com>
2023-07-22 14:25:31 +0200

    Implemented Change Requests: Renamed RunInitAsync() to RunWithInitAsync() Added TeardownAsync() Updated comments and exception descriptions Added .ConfigureAwait(false) to all await calls

--- src/Extensions.Hosting.AsyncInitialization/Hosting/AsyncInitializationHostExtensions.cs
+++ src/Extensions.Hosting.AsyncInitialization/Hosting/AsyncInitializationHostExtensions.cs
@@ -18,33 +18,55 @@ namespace Microsoft.Extensions.Hosting
         /// <param name="host">The host.</param>
         /// <param name="cancellationToken">Optionally propagates notifications that the operation should be cancelled</param>
         /// <returns>A task that represents the initialization completion.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when the host is null.</exception>
+        /// <exception cref="InvalidOperationException">Thrown when the initialization service has not been registered.</exception>
+        /// <exception cref="OperationCanceledException">Thrown when the cancellationToken is cancelled.</exception>
         public static async Task InitAsync(this IHost host, CancellationToken cancellationToken = default)
         {
             if (host == null)
                 throw new ArgumentNullException(nameof(host));
 
             using var scope = host.Services.CreateScope();
-            var rootInitializer = scope.ServiceProvider.GetService<RootInitializer?>();
-            if (rootInitializer == null)
-            {
-                throw new InvalidOperationException("The async initialization service isn't registered, register it by calling AddAsyncInitialization() on the service collection or by adding an async initializer.");
-            }
+            var rootInitializer = scope.ServiceProvider.GetService<RootInitializer>()
+                ?? throw new InvalidOperationException("The async initialization service isn't registered, register it by calling AddAsyncInitialization() on the service collection or by adding an async initializer.");
 
-            await rootInitializer.InitializeAsync(cancellationToken);
+            await rootInitializer.InitializeAsync(cancellationToken).ConfigureAwait(false);
         }
 
-
         /// <summary>
-        /// Initializes and runs the application, by first calling all registered async initializers.
-        /// Upon completion, any registered initializers implementing <see cref="IAsyncTeardown"/> are called.
+        /// Performs application teardown by calling all registered async initializers implementing <see cref="IAsyncTeardown"/>.
         /// </summary>
         /// <param name="host">The host.</param>
         /// <param name="cancellationToken">Optionally propagates notifications that the operation should be cancelled</param>
-        /// <returns>A task that represents the initialization completion.</returns>
-        /// <exception cref="ArgumentNullException"></exception>
-        /// <exception cref="InvalidOperationException"></exception>
-        /// <exception cref="OperationCanceledException"></exception>
-        public static async Task RunInitAsync(this IHost host, CancellationToken cancellationToken = default)
+        /// <returns>A task that represents the teardown completion.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when the host is null.</exception>
+        /// <exception cref="InvalidOperationException">Thrown when the initialization service has not been registered.</exception>
+        /// <exception cref="OperationCanceledException">Thrown when the cancellationToken is cancelled.</exception>
+        /// <exception cref="ObjectDisposedException">Thrown when the host has been disposed.</exception>
+        /// <remarks>Make sure not to call this method after the host has terminated, eg. after a call to RunAsync().</remarks>
+        public static async Task TeardownAsync(this IHost host, CancellationToken cancellationToken = default)
+        {
+            if (host == null)
+                throw new ArgumentNullException(nameof(host));
+
+            using var scope = host.Services.CreateScope();
+            var rootInitializer = scope.ServiceProvider.GetService<RootInitializer>()
+                ?? throw new InvalidOperationException("The async initialization service isn't registered, register it by calling AddAsyncInitialization() on the service collection or by adding an async initializer.");
+
+            await rootInitializer.TeardownAsync(cancellationToken).ConfigureAwait(false); 
+        }
+
+        /// <summary>
+        /// Initializes and runs the application, by first calling all registered async initializers.
+        /// After the host terminates, any registered initializers that implement <see cref="IAsyncTeardown"/> are called to perform the teardown.
+        /// </summary>
+        /// <param name="host">The host.</param>
+        /// <param name="cancellationToken">Optionally propagates notifications that the operation should be cancelled</param>
+        /// <returns>A task that represents the run completion.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when the host is null.</exception>
+        /// <exception cref="InvalidOperationException">Thrown when the initialization service has not been registered.</exception>
+        /// <exception cref="OperationCanceledException">Thrown when the cancellationToken is cancelled.</exception>
+        public static async Task RunWithInitAsync(this IHost host, CancellationToken cancellationToken = default)
         {
             if (host == null) throw new ArgumentNullException(nameof(host));
 
